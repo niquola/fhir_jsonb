f98b40a33c0d9f35b886f5f2283e860d94e0c939
Danil Kutkevich <danil@kutkevich.org>
2014-05-12 12:21:12 +0000

    add some tests stubs

--- sql/fhir_jsonb.sql
+++ sql/fhir_jsonb.sql
@@ -38,6 +38,34 @@ doc #>> '{reason,text}'
 FROM encounters
 ORDER BY doc #>> '{reason}';
 
+SELECT
+  doc#>'{participant,0}' as part,
+  doc#>'{class}' as cls,
+  doc#>'{status}' as status
+FROM encounters
+WHERE
+doc @@
+'("class" = "emergency" &
+  "participant".#."individual" (
+    "reference" = "Galen" &
+    "type".#."coding".# (
+      "code" && ["ADM", "ATND"] &
+      "system" = "encounter-participant-type")) &
+  !("status" && ["planned", "finished", "cancelled"]))'
+LIMIT 10;
+
+SELECT count(*)
+FROM encounters
+WHERE
+doc @@
+'("class" = "emergency" &
+  "participant".#."individual" (
+    "reference" = "Galen" &
+    "type".#."coding".# (
+      "code" && ["ADM", "ATND"] &
+      "system" = "encounter-participant-type")) &
+  !("status" && ["planned", "finished", "cancelled"]))';
+
 SELECT count(*)
 FROM observations;
 
@@ -50,6 +78,18 @@ SELECT DISTINCT ON (doc #>> '{status}')
 doc #>> '{status}'
 FROM observations
 ORDER BY doc #>> '{status}';
+
+SELECT count(*) FROM observations
+WHERE doc @@ '(
+  "name"."coding".# (
+    "system" = "http://loinc.org"
+     &
+    "code" = "8310-5"
+  )
+)'
+AND
+(doc #>> '{appliesPeriod,start}')::timestamp > CURRENT_DATE - INTERVAL '3 month'
+LIMIT 10;
 
 SELECT count(*)
 FROM conditions;
@@ -68,3 +108,35 @@ SELECT DISTINCT ON (doc #>> '{status}')
 doc #>> '{status}'
 FROM conditions
 ORDER BY doc #>> '{status}';
+
+SELECT
+doc#>'{status}',
+doc#>'{category,coding}',
+doc#>'{code,coding}'
+FROM conditions
+LIMIT 10;
+
+SELECT count(*)
+FROM conditions
+WHERE
+doc @@
+'("status" = "confirmed" &
+  "category"."coding".# (
+    "system" = "http://snomed.info/sct" &
+    "code" = "diagnosis") &
+  "code"."coding".#."system" = "http://snomed.info/sct")'
+AND (doc #>> '{code,coding,0,code}' ) in  (SELECT code from stroke_diagnoses) ;
+
+SELECT count(*)
+FROM conditions
+WHERE
+doc @@
+'("status" = "confirmed" &
+  "category"."coding".# (
+    "system" = "http://snomed.info/sct" &
+    "code" = "diagnosis") &
+  "code"."coding".# (
+    "system" = "http://snomed.info/sct" &
+    "code" in ("433.01", "433.10", "433.11", "433.21", "433.31", "433.81", "433.91", "434.00", "434.01", "434.11", "434.91", "436", "430", "431")
+  ))'
+;
